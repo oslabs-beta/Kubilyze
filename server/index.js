4267a2c4232e6bca6336ac4b0f3d726931fcb9a3
aissata <nutzela@gmail.com>
2024-05-15 12:18:08 -0400

    first dev commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+//Boilerplate server file
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+//Serve files
+// app.use(express.static(path.resolve(__dirname, '../dist')));
+
+//Glolbal error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+module.exports = app.listen(port, () =>
+  console.log(`Listening on port ${port}`)
+);
