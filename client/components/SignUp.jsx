3e6a98fba5b6f1d4d592fd1b70edd90fa816fa6d
ReefHall <120200034+ReefHall@users.noreply.github.com>
2024-05-23 14:08:36 -0400

    Update SignUp.jsx
    import was missing for navbar in signup component

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
+import Navbar from "./Navbar.jsx"
 
 export default function SignUp() {
   const navigate = useNavigate();
