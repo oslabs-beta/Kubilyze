8ca27da8ba5e140262cb03c59a9760af0714678d
mayagoyle <mayagoyle99@gmail.com>
2024-06-05 12:20:45 -0400

    sidebar fixes

--- client/components/AddCluster.jsx
+++ client/components/AddCluster.jsx
@@ -2,35 +2,45 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import NavbarDash from "./NavbarDash.jsx";
 
-export default function AddCluster({username, setClusterName, setCluster, setNodes}) {
-  const [accessInfo, setAccessInfo] = useState({accesskey: '', secretkey: '', sessiontoken: '', region: ''})
+export default function AddCluster({
+  username,
+  setClusterName,
+  setCluster,
+  setNodes,
+}) {
+  const [accessInfo, setAccessInfo] = useState({
+    accesskey: "",
+    secretkey: "",
+    sessiontoken: "",
+    region: "",
+  });
   const navigate = useNavigate();
   const [modalOpen, setModalOpen] = useState(false);
-  
+
   const handleUserInput = (e) => {
-    setAccessInfo({...accessInfo, [e.target.name]: e.target.value})
-  }
+    setAccessInfo({ ...accessInfo, [e.target.name]: e.target.value });
+  };
 
   const addCredentials = () => {
-    fetch('http://localhost:3000/user/credentials', {
-      method: 'POST',
+    fetch("http://localhost:3000/user/credentials", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({...accessInfo, username})
+      body: JSON.stringify({ ...accessInfo, username }),
     })
-    .then((response)=> {
-      if(response.ok) return response.json()
-        console.log('an error happened in the server')
-    })
-    .then(user=> {
-      if(user) {
-        console.log(user)
-        navigate("/selectcluster");
-      }
-      setAccessInfo({accesskey: '', secretkey: '', sessiontoken: ''})
-    })
-    .catch(e=> console.log(e))
+      .then((response) => {
+        if (response.ok) return response.json();
+        console.log("an error happened in the server");
+      })
+      .then((user) => {
+        if (user) {
+          console.log(user);
+          navigate("/selectcluster");
+        }
+        setAccessInfo({ accesskey: "", secretkey: "", sessiontoken: "" });
+      })
+      .catch((e) => console.log(e));
   };
 
   const toggleModal = () => {
@@ -39,7 +49,12 @@ export default function AddCluster({username, setClusterName, setCluster, setNod
 
   return (
     <>
-    <NavbarDash username={username} setClusterName={setClusterName} setCluster={setCluster} setNodes={setNodes}/>
+      <NavbarDash
+        username={username}
+        setClusterName={setClusterName}
+        setCluster={setCluster}
+        setNodes={setNodes}
+      />
       <div className="entirepage">
         <div className="loginContainer">
           <div id="loginform">
@@ -80,9 +95,9 @@ export default function AddCluster({username, setClusterName, setCluster, setNod
                 onChange={handleUserInput}
               />
             </div>
-            
+
             <div className="readMore">
-              <button onClick={toggleModal}>Read More</button>
+              <button onClick={toggleModal}>Where do I find this info?</button>
             </div>
             <div className="submit">
               <button onClick={addCredentials}>Submit</button>
@@ -97,11 +112,23 @@ export default function AddCluster({username, setClusterName, setCluster, setNod
               &times;
             </span>
             <h2>How to add your EKS Clusters </h2>
+
+            <br></br>
             <p>
-              1- go to your AWS account
+              Please make sure to follow the steps of the ReadMe closely. You
+              can find the ReadMe linked in the Docs on the homepage.
             </p>
+            <br></br>
             <p>
-              2- step 1
+              Make sure to create a role in your AWS account and have an IAM
+              user assume that role so that you're inputting those temporary
+              creditionals into the app.
+            </p>
+            <br></br>
+            <p>
+              Please note that the credentials have a default time limit of 1
+              hour unless you indicate otherwise. Your credentials will expire
+              after that duration and you will need to regenerate tokens.
             </p>
           </div>
         </div>
